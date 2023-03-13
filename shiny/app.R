d881c5b25ab05eef7809d61375629d873c286f0d
katmackay <k_mackay@ucsb.edu>
2023-03-13 10:29:08 -0700

    avoiding conflicts

--- shiny/app.R
+++ shiny/app.R
@@ -129,7 +129,7 @@ ui <- fluidPage(theme = my_theme,
                 navbarPage("Coral Across Northshore Moorea",
                            tabPanel('About',
                                     mainPanel(
-                                      h1('Overview of the Study'),
+                                      h1(typed::typedOutput('Overview of the Study')),
                                       h5("This shiny app showcases the resilience of Moorea's outer reef communities to changing ocean conditions over the past decade, despite the acidic ocean conditions and rising ocean temperatures. The app provides data from coral surveys conducted in the Northshore lagoon in Moorea, where 5 5x5m transects were set up at 16 sites to measure branching (pocillopora and Acropora) corals' lengths and available settlement space in each plot. The app aims to help understand the spatial distribution of coral taxa and size structure to further understand community dynamics and identify areas of efficient out-planting sites and optimal habitat for restoration efforts that are in Moorea. The study suggests that the recovery of outer reef coral communities around Moorea may include an increased capacity to respond to future conditions due to the diversity of coral recruits, at least among pocillopora species."),
                                       h6('Alicia Canales, Danielle Hoekstra, Kat Mackay')
                                     )),
@@ -160,8 +160,7 @@ ui <- fluidPage(theme = my_theme,
                                       textInput(inputId = "width",
                                         label = "Width"),
                                       submitButton("Analyze!"),
-                                      '
-This bar plot presents the probability that an undetermined or new Moorea coral is pocillopora or acropora. The user input values are applied to a binomial logistic regression that we have trained using the rest of the coral data set. Based on those values we can predict the likelihood of if the unknown coral is species pocillopora or acropora.'),
+                                      'This bar plot presents the probability that an undetermined or new Moorea coral is pocillopora or acropora. The user input values are applied to a binomial logistic regression that we have trained using the rest of the coral data set. Based on those values we can predict the likelihood of if the unknown coral is species pocillopora or acropora.'),
                            
                                     mainPanel('Prediction Results',
                            plotOutput('bar')),
